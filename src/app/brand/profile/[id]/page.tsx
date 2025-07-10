15f24ad49e597f8d3e5b4ae41da27c07f9539b26
Cleeein <pagaroganjhonclein@gmail.com>
2025-07-10 10:02:20 +0800

    fix: potential error useEffect hook not supervised

--- src/app/brand/profile/[id]/page.tsx
+++ src/app/brand/profile/[id]/page.tsx
@@ -329,7 +329,7 @@ export default function BrandProfilePage() {
 
   useEffect(() => {
     if (role === "brand") setisBrand(true);
-  });
+  }, []);
 
   useEffect(() => {
     // Simulate API call
